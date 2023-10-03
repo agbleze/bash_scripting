28f06b67e5883fdf206e9ddc51a6efb1132ef39b
Agbleze <62272559+agbleze@users.noreply.github.com>
2023-10-03 22:57:17 +0200

    Add files via upload

---
+++ checking_files_dir.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mydir=/Users/lin/Documents/python_venvs/cv_with_roboflow_data
+
+if [ -d $mydir ]
+then 
+echo "Dir $mydir already exists"
+else
+echo "Dir $mydir does not exist"
+fi
+
