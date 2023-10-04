661c6a22002643fc1bdab37962df77f42b4e4a76
Agbleze <62272559+agbleze@users.noreply.github.com>
2023-10-04 22:03:27 +0200

    Add files via upload

---
+++ run_mysql.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read -p "MySQL User: " user_name
+read -sp "MySQL Password: " mysql_pwd
+echo
+read -p "MySQL Command: " mysql_cmd
+read -p "MySQL Database: " mysql_db
+mysql -u "$user_name" -p $mysql_pwd $mysql_db -Be "$mysql_cmd"
