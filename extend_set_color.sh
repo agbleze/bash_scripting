6d76d657b7d647b2fc348d6fbe1201ddcaca6c1a
Agbleze <62272559+agbleze@users.noreply.github.com>
2023-10-24 21:04:54 +0200

    Add files via upload

--- extend_set_color.sh
+++ extend_set_color.sh
@@ -4,9 +4,14 @@ source ./set_color.sh
 
 if [ $# -lt 1 ]; then
     echo -e "${RED}Usage: $0 <name>$RESET"
-    exit 1
+    #exit 1
 fi
 echo -e "${GREEN}Hello $1$RESET"
 #exit 0
 
 echo 'Welcome to shell scripting! first. Happy coding lin'
+
+echo -e "Check directory at ${GREEN}$PWD ${RESET} and ${BLUE} provide your files"
+
+
+
